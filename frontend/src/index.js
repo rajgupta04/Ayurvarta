f8e8ec2e20cd6f15984ab13685be0b717efd37f3
rajgupta04 <engineerrajgupta@gmail.com>
2026-03-09 12:00:00 +0530

    Update s:\Workplace\Ayurvarta-repo\frontend\src\index.js on 2026-03-09

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -14,4 +14,10 @@ root.render(
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vi
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
+// Commit on 2026-03-13 
+// Commit on 2026-03-13 
+// Commit on 2026-03-13 
+// Commit on 2026-03-09 
